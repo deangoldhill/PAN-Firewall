3d44ff96e91c0999820388ef9bbf6f4f03c2721b
deangoldhill <deangoldhill@gmail.com>
2026-02-27 21:57:38 +0000

    Update const.py

--- custom_components/pan_firewall/const.py
+++ custom_components/pan_firewall/const.py
@@ -8,7 +8,10 @@ CONF_USERNAME = "username"
 CONF_PASSWORD = "password"
 CONF_VSYS = "vsys"
 CONF_VERIFY_SSL = "verify_ssl"
+CONF_SCAN_INTERVAL = "scan_interval"          # ← NEW
 
 DEFAULT_PORT = 443
 DEFAULT_VSYS = "vsys1"
 DEFAULT_VERIFY_SSL = True
+DEFAULT_SCAN_INTERVAL = 30
+MIN_SCAN_INTERVAL = 10
