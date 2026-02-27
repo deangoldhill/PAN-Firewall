09525b713b7a88636def3faa1a98e41f2d47f8d3
deangoldhill <deangoldhill@gmail.com>
2026-02-27 17:40:07 +0000

    Create const.py

---
+++ custom_components/pan_firewall/const.py
@@ -0,0 +1,14 @@
+"""Constants for the PAN Firewall integration."""
+
+DOMAIN = "pan_firewall"
+
+CONF_HOST = "host"
+CONF_PORT = "port"
+CONF_USERNAME = "username"
+CONF_PASSWORD = "password"
+CONF_VSYS = "vsys"
+CONF_VERIFY_SSL = "verify_ssl"
+
+DEFAULT_PORT = 443
+DEFAULT_VSYS = "vsys1"
+DEFAULT_VERIFY_SSL = True
