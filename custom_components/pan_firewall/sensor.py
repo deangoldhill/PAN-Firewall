4c130525454517b6a25978f156e5d768e34371c3
deangoldhill <deangoldhill@gmail.com>
2026-03-06 15:48:28 +0000

    Update sensor.py

--- custom_components/pan_firewall/sensor.py
+++ custom_components/pan_firewall/sensor.py
@@ -2,9 +2,9 @@
 
 from homeassistant.components.sensor import SensorEntity, SensorStateClass
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity import EntityCategory
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
-from homeassistant.helpers.entity import EntityCategory
 from homeassistant.helpers import device_registry as dr
 
 from .const import DOMAIN
